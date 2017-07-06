29ef74b0a67b5d967eb8d9b200aea138689f153a
knguyenct <knguyen.ct@gmail.com>
2017-07-06 00:35:51 -0700

    Initial first unit test

---
+++ test-jenkins/JenkinsUnitTest/UnitTestFibo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test_jenkins;
+
+namespace JenkinsUnitTest
+{
+    [TestClass]
+    public class UnitTestFibo
+    {
+        [TestMethod]
+        public void TestGenerator()
+        {
+            Assert.IsNotNull(Fibonacci.Generate(5));
+        }
+    }
+}
---
+++ test-jenkins/test-jenkins/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿namespace test_jenkins
+{
+    public static class Fibonacci
+    {
+        public static int Generate(int n)
+        {
+            if(n < 3)
+            {
+                return 1;
+            }
+            int a = 1;
+            int b = 1;
+            for (var i = 3; i <= n; ++i)
+            {
+                var t = a + b;
+                a = b;
+                b = t;
+            }
+            return b;
+        }
+    }
+}
